35ec65e6fa78c1b94f21d9a41175244e24a6428f
StephenMwash <stephenthiongo77@gmail.com>
2026-09-14 17:22:08 +0300

    Added imports to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,12 @@ import {BrowseRouter, Routes, Route} from "react-router-dom";
 import Login from "./Pages/auth/Login";
 import Register from "./Pages/auth/Register";
 import AdminDashBoard from "./Pages/Admin/AdminDashboard";
+import AssignDelivery from "./Pages/Admin/AssignDelivery";
+import Orders from "./Pages/Admin/Orders";
+import Users from "./Pages/Admin/Users";
+import AssignedOrders from "./Pages/Delivery/AssignedOrders";
+import DeliveryDashboard from "./Pages/Delivery/DeliveryDashboard";
+import UpdateStatus from "./Pages/Delivery/UpdateStatus";
 
 
 function App(){
